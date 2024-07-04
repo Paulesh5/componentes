bdf6352546d2410ce1b6937363823a91ebd7104e
Paul Hidalgo <paulesh5@gmail.com>
2024-07-04 12:28:53 -0500

    5 lugares

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# componentes
+# Componentes
 
-A new Flutter project.
+Lugares turísticos
 
-## Getting Started
+## Autor
 
-This project is a starting point for a Flutter application.
+- [@Paulesh5](https://www.github.com/Paulesh5)
 
-A few resources to get you started if this is your first Flutter project:
+## Capturas
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
+![alt text](image-4.png)
--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,106 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
+    const String appTitle = 'Lugares turísticos';
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(appTitle),
+          title: const Center(child: Text(appTitle)),
         ),
         body: const SingleChildScrollView(
           child: Column(
             children: [
-              TitleSection(name: 'La Ronda', location: 'Quito, Ecuador'),
               ImageSection(
-                image: 'images/lake.jpg',
+                image: 'images/laRonda.jpg',
               ),
+              TitleSection(name: 'La Ronda', location: 'Quito, Ecuador'),
               ButtonSection(),
-              TextSection(description: 'Descripcion de La Ronda'),
-              TitleSection(name: 'Basilica', location: 'Quito, Ecuador'),
+              TextSection(
+                description:
+                  'La Ronda es una de las calles más antiguas y pintorescas '
+                  'de Quito, Ecuador. Ubicada en el centro histórico de la '
+                  'ciudad, esta vía empedrada y estrecha es conocida por sus '
+                  'coloridas casas coloniales, balcones adornados con flores '
+                  'y faroles antiguos. La Ronda ha sido revitalizada como un '
+                  'atractivo turístico, ofreciendo una variedad de restaurantes, '
+                  'cafés, galerías de arte y tiendas de artesanías. Es un lugar '
+                  'ideal para disfrutar de la cultura y la historia ecuatoriana, '
+                  'especialmente en las noches, cuando se llena de vida con música '
+                  'en vivo y eventos culturales.',
+                ),
+              ImageSection(
+                image: 'images/basilica.jpg',
+              ),
+              TitleSection(name: 'La Basílica del Voto Nacional', location: 'Quito, Ecuador'),
               ButtonSection(),
-              TextSection(description: 'Descripcion de la Basilica'),
+              TextSection(
+                description: 
+                  'La Basílica del Voto Nacional, ubicada en Quito, Ecuador, '
+                  'es una impresionante iglesia neogótica y uno de los '
+                  'edificios religiosos más grandes de América Latina. '
+                  'Construida a finales del siglo XIX y principios del XX, '
+                  'destaca por su arquitectura imponente, con altas torres y '
+                  'detalles ornamentales que incluyen gárgolas en forma de '
+                  'animales ecuatorianos. Los visitantes pueden subir a las '
+                  'torres para disfrutar de vistas panorámicas de Quito y su '
+                  'entorno. La Basílica es un símbolo importante de la devoción '
+                  'católica en Ecuador y un atractivo turístico significativo en '
+                  'el centro histórico de la ciudad.'
+                ),
+              ImageSection(
+                image: 'images/mitadDelMundo.jpg',
+              ),
               TitleSection(name: 'Mitad del Mundo', location: 'Quito, Ecuador'),
               ButtonSection(),
-              TextSection(description: 'Descripcion de la Mitad del Mundo'),
-              TitleSection(name: 'Panescillo', location: 'Quito, Ecuador'),
+              TextSection(
+                description: 
+                  'La Mitad del Mundo es un monumento y complejo turístico '
+                  'ubicado cerca de Quito, Ecuador. Marca el punto donde se '
+                  'cree que la línea ecuatorial divide el hemisferio norte del '
+                  'hemisferio sur. El sitio principal es el Monumento a la Mitad '
+                  'del Mundo, una torre de 30 metros de altura coronada con un '
+                  'globo terrestre. Alrededor del monumento hay un museo etnográfico, '
+                  'tiendas de souvenirs, restaurantes y diversas exhibiciones '
+                  'interactivas. Es un destino popular para turistas que buscan '
+                  'tomarse fotos con un pie en cada hemisferio y aprender más '
+                  'sobre la geografía y cultura ecuatoriana.'
+                ),
+              ImageSection(
+                image: 'images/panecillo.jpg',
+              ),
+              TitleSection(name: 'El Panecillo', location: 'Quito, Ecuador'),
               ButtonSection(),
-              TextSection(description: 'Descripcion del Panescillo'),
-              TitleSection(name: 'Plaza Foch', location: 'Quito, Ecuador'),
+              TextSection(
+                description: 
+                  'El Panecillo es una colina ubicada en el centro de Quito, '
+                  'Ecuador, que ofrece vistas panorámicas de la ciudad. En su '
+                  'cima se encuentra la icónica estatua de la Virgen de Quito, '
+                  'una monumental figura de aluminio de la Virgen María con alas, '
+                  'que mide 45 metros de altura. El Panecillo es un lugar popular '
+                  'para turistas y locales, ideal para apreciar la arquitectura '
+                  'colonial de Quito y las montañas circundantes. Además de su '
+                  'valor turístico, tiene importancia histórica y cultural, siendo '
+                  'un punto de referencia en la ciudad.'
+                ),
+              ImageSection(
+                image: 'images/foch.jpg',
+              ),
+              TitleSection(name: 'La Plaza Foch', location: 'Quito, Ecuador'),
               ButtonSection(),
-              TextSection(description: 'Descripcion de la Plaza Foch')
+              TextSection(
+                description: 
+                  'La Plaza Foch, ubicada en el barrio La Mariscal de Quito, '
+                  'Ecuador, es conocida como el epicentro de la vida nocturna '
+                  'y turística de la ciudad. Rodeada de numerosos bares, '
+                  'restaurantes, discotecas y cafés, es un lugar vibrante y '
+                  'animado, especialmente durante las noches y los fines de semana. '
+                  'La plaza es un punto de encuentro popular tanto para turistas '
+                  'como para locales, ofreciendo una amplia variedad de opciones '
+                  'de entretenimiento y gastronomía. Su ambiente cosmopolita y '
+                  'seguro la convierte en un destino atractivo para quienes buscan '
+                  'disfrutar de la vida social y cultural de Quito.'
+                )
             ],
           ),
         ),
